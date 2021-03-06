b6304dd77c4bead797ebf6b228c02eecb74aa697
betsy_manuel <betsy.manuel03@gmail.com>
2021-03-05 17:43:13 -0800

    knexfile edited

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ require('dotenv').config({path: './.env'});
 module.exports = {
     client: 'pg',
     debug: true,
-    connection: DATBASE_URL,
+    connection: "postgres://mreafmenwkgmod:a0d1fd20d74d41ef965a7d1603d4c0e8b17d33185516bdf513a55321d518f123@ec2-34-239-33-57.compute-1.amazonaws.com:5432/d33opsq5pvq4b9",
     migrations:{
       tableName:"migrations"
     },
