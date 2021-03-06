12dc2a302655879020b76088bf316f9e38065958
betsy_manuel <betsy.manuel03@gmail.com>
2021-03-05 17:58:14 -0800

    modified knexfile

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,7 @@
 
 require('dotenv').config({path: './.env'});
 module.exports = {
+  production:{
     client: 'pg',
     debug: true,
     connection: "postgres://mreafmenwkgmod:a0d1fd20d74d41ef965a7d1603d4c0e8b17d33185516bdf513a55321d518f123@ec2-34-239-33-57.compute-1.amazonaws.com:5432/d33opsq5pvq4b9",
@@ -12,4 +13,23 @@ module.exports = {
     ssl: {
       rejectUnauthorized: false
     } 
+  },
+  staging: {
+    client: 'pg',
+    connection: {
+      url: "postgres://mreafmenwkgmod:a0d1fd20d74d41ef965a7d1603d4c0e8b17d33185516bdf513a55321d518f123@ec2-34-239-33-57.compute-1.amazonaws.com:5432/d33opsq5pvq4b9",
+      charset: 'utf8',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory:"./db/migrations"
+    },
+  },
+
+  
+
 }
