00dd035e98ae81b012c46140d641008485036ec4
betsy_manuel <betsy.manuel03@gmail.com>
2021-03-05 17:45:16 -0800

    modified knexfile

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,8 @@ module.exports = {
     debug: true,
     connection: "postgres://mreafmenwkgmod:a0d1fd20d74d41ef965a7d1603d4c0e8b17d33185516bdf513a55321d518f123@ec2-34-239-33-57.compute-1.amazonaws.com:5432/d33opsq5pvq4b9",
     migrations:{
-      tableName:"migrations"
+      tableName:"migrations",
+      directory:"./db/migrations"
     },
     ssl: true 
 }
